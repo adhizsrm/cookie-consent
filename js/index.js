a20d3c1c7ec533b8726f61cd73648dad47d18ca1
adhizsrm <adhizsrm@gmail.com>
2025-01-29 18:16:14 +0530

    adding further modifications to the modal dialogue

--- js/index.js
+++ js/index.js
@@ -19,5 +19,19 @@ consentForm.addEventListener("submit",function(e){
         <img src="images/loading.svg" class="loading">
         <p id="uploadText">Uploading your data to the dark web...</p>
     </div>
-    `
+    `;
+    setTimeout(function(){
+        document.getElementById('uploadText').innerText = `Making the sale...`;
+    },1500);
+
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = 
+        `
+        <h2>Thanks you sucker!</h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+        `;
+    },3000);
 })
